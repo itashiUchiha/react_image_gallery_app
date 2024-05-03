1dafb46c7313c9e1d63db5043d014959d0ac6cd1
cheikh <cheikhsidibe13@gmail.com>
2024-05-03 10:55:10 +0000

    finalise modal and paralle route

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,12 +1,24 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImageById } from "~/server/queries";
 export  default async function FullImageView(props:{id:number}) {
   const image =  await getImageById(props.id);
+  const uploaderInfo= await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex w-full h-full ">
-        <div className="flex flex-col ">
-            <img src={image.url} className="object-contain  overflow-hidden"/>
+        <div className="flex flex-col items-center justify-center">
+            <img src={image.url} className="object-contain flex-shrink overflow-hidden"/>
         </div>
-        <div className="flex flex-col w-48 border-l">{image.name}</div>
+        <div className="flex flex-col  flex-shrink-0 w-48 border-l ">
+            <div className="border-b text-lg text-center p-2 ">{image.name}</div>
+            <div className="flex flex-col p-2">
+                <span>Uploaded by</span>
+                <span className="text-lg">{uploaderInfo.fullName}</span>
+            </div>
+            <div className="flex flex-col p-2">
+                <span>created at </span>
+                <span className="text-lg">{image.createdAt.toLocaleDateString()}</span>
+            </div>
+        </div>
     </div>
     
 );
